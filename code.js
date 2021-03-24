e113021543a44b89e95af68185c4f786d40fa27a
irisdev06 <ivgb_@outlook.com>
2021-03-23 23:33:00 -0500

    js

---
+++ code.js
@@ -0,0 +1,10 @@
+  
+const onLoad = () => {
+    const colorPicker = document.getElementById("nail-color");
+    colorPicker.addEventListener("input", changeColor, false);
+}
+
+const changeColor = (event) => {
+    const element = document.getElementById("nails");
+    element.style.backgroundColor = event.target.value;
+}
